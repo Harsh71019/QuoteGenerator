998827cca192afb07e3e0eb77bc5f2e46648504f
harsh <harsh710pat@gmail.com>
2020-07-25 00:23:26 +0530

    Final Commit Maybe Added Loader

--- script.js
+++ script.js
@@ -3,9 +3,28 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById('loader')
+
+//Loader Logic
+
+function loading(){
+    loader.hidden =false;
+    quoteContainer.hidden=true;
+}
+
+//Hide Loading 
+
+function complete () {
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
 
 // Get Quote from API
 async function getQuote() {
+    loading();
   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -28,8 +47,11 @@ async function getQuote() {
     }
 
     quoteText.innerText = data.quoteText;
+    //Stop Loader and show Quote
+    complete()
   } catch {
     getQuote();
+
   }
 }
 
@@ -52,3 +74,4 @@ twitterBtn.addEventListener('click',tweetQuote);
 
 //on Load
 getQuote();
+// loading()
